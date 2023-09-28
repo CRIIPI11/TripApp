636a3c09cf0fefa7c1b4e2257944d49a1e052bec
Cristhian Molina <cdml2001@hotmail.com>
2023-09-27 21:34:50 -0400

    feat: Added location permission to app

---
+++ hooks/useLocationStore.js
@@ -0,0 +1,57 @@
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  onLogin,
+  onSetErrorMsg,
+  onSetLoadingLocation,
+} from "../redux/location/locationSlice";
+
+export const useUserStore = () => {
+  const dispatch = useDispatch();
+  const { location, loading, errorMsg } = useSelector(
+    (state) => state.location
+  );
+
+  const getLocation = async () => {
+    const { status } = await requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      dispatch(onLogin({ permission: status }));
+      dispatch(onSetErrorMsg("Permission to access location was denied"));
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    dispatch(
+      onLogin({
+        location: {
+          lat: location.coords.latitude,
+          long: location.coords.longitude,
+        },
+        permission: status,
+      })
+    );
+  };
+
+  const startLocation = async () => {
+    dispatch(onSetLoadingLocation(true));
+    try {
+      await getLocation();
+    } catch (error) {
+      console.log(error);
+      dispatch(onSetErrorMsg(error.message));
+    }
+  };
+
+  return {
+    //properties
+    location,
+    loading,
+    errorMsg,
+    //methods
+    startLocation,
+  };
+};
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import infoSlice from "./infoSlice";
 import filterSlice from "./filterSlice";
+import { locationSlice } from "./location/locationSlice";
 
 export const store = configureStore({
   reducer: {
     info: infoSlice,
     filter: filterSlice,
+    location: locationSlice.reducer,
   },
 });
