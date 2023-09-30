1289dfb9b3af51dc73514ed800348601ec7f1565
EliMojica <145515185+EliMojica@users.noreply.github.com>
2023-09-30 03:07:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import "expo-router/entry";
-//import {APPLICATION_ID, JAVASCRIPT_ID} from ".env";
+import {APPLICATION_ID, JAVASCRIPT_ID} from ".env";
 import Parse from "parse/react-native.js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 //Initializing the SDK
 Parse.setAsyncStorage(AsyncStorage);
-Parse.initialize("4MpttAlZR59kHVpZJhWkMvYCOPne3fjWgmORo2dG", "zBEyWz8qrTK3H6kF1DfwIRrjHx6wj5rGVjxjUwwE");
+Parse.initialize(APPLICATION_ID, JAVASCRIPT_ID);
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 console.log("this is a test");
