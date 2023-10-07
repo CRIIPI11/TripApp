717d347abb8f74b2c17ac53f0166d9a08f959b37
Cristhian Molina <cdml2001@hotmail.com>
2023-10-07 15:35:43 -0400

    fix: Fix end-point typo

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,91 +1,99 @@
-const express = require('express')
-const AsyncStorage = require('@react-native-async-storage/async-storage')
-const Parse = require('parse/node')
-const router = express.Router()
+const express = require("express");
+const AsyncStorage = require("@react-native-async-storage/async-storage");
+const Parse = require("parse/node");
+const router = express.Router();
 //create end
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Initialize Parse SDK with Back4App's credentials
 Parse.setAsyncStorage(AsyncStorage);
 Parse.initialize(process.env.B4A_APPLICATION_ID, process.env.B4A_JAVASCRIPT_ID);
 Parse.serverURL = "https://parseapi.back4app.com/";
 
-Parse.User.enableUnsafeCurrentUser()
+Parse.User.enableUnsafeCurrentUser();
 
-router.get('/signup',(req, res) => {
-    const user = new Parse.User();
+router.get("/signup", (req, res) => {
+  const user = new Parse.User();
 
-    console.log(req.body)
-    user.set("username", req.body.email)
-    user.set("password", req.body.password)
-    user.set("email", req.body.email)
-    user.set("name", req.body.name)
+  console.log(req.body);
+  user.set("username", req.body.email);
+  user.set("password", req.body.password);
+  user.set("email", req.body.email);
+  user.set("name", req.body.name);
 
-    user.signUp().then((obj)=>{
-       res.json({"result": "success"})
-    }).catch((err)=>{
-        console.log(error)   
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
-    }) 
+  user
+    .signUp()
+    .then((obj) => {
+      res.json({ result: "success" });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
 });
 
-router.post('/login', (req, res) => {
-    const username = req.body.email
-    const password = req.body.password
+router.post("/login", (req, res) => {
+  const username = req.body.email;
+  const password = req.body.password;
 
-    Parse.User.logIn(username, password).then((obj)=>{
-        res.json({
-            "result": "success"
-        })
-     }).catch((error)=>{
-        console.log(error)
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
+  Parse.User.logIn(username, password)
+    .then((obj) => {
+      res.json({
+        result: "success",
+      });
     })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
 });
 
-router.post('/logout', (req, res)=> {   
-    Parse.User.logOut(currentUser).then(() => {
-        res.send('User logged out successfully');
-    }).catch((error)=> {
-        console.error('Error logging out user', error)
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
+router.post("/logout", (req, res) => {
+  Parse.User.logOut(currentUser)
+    .then(() => {
+      res.send("User logged out successfully");
     })
-})
+    .catch((error) => {
+      console.error("Error logging out user", error);
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
+});
 
-router.get('/me', (req, res) => {
-    const currentUser = Parse.User.current();
- 
-    if (currentUser) {  
-        const {username, email, name, preferences, tripsCounter, placesVisited} = currentUser.toJSON()    
-        const responseObj = {
-            username,
-            email,
-            name,
-            preferences,
-            tripsCounter,
-            placesVisited
-        }
-        res.json(responseObj);
-    } else {
-        res.json({
-            "result": "failure", 
-            "message": "No user logged in"
-        })
-    }
+router.get("/me", (req, res) => {
+  const currentUser = Parse.User.current();
+
+  if (currentUser) {
+    const { username, email, name, preferences, tripsCounter, placesVisited } =
+      currentUser.toJSON();
+    const responseObj = {
+      username,
+      email,
+      name,
+      preferences,
+      tripsCounter,
+      placesVisited,
+    };
+    res.json(responseObj);
+  } else {
+    res.json({
+      result: "failure",
+      message: "No user logged in",
+    });
+  }
 });
 
 //update user preferences
-//structure of the request body below: 
+//structure of the request body below:
 // {
 //     "preferences": {
 //       "artAndCulture": true,
@@ -98,85 +106,97 @@ router.get('/me', (req, res) => {
 //     }
 // }
 
-router.put('/update/preferences', (req, res) =>{
-    const user = Parse.User.current()
+router.put("/update/preferences", (req, res) => {
+  const user = Parse.User.current();
 
-    if(!user){
-        res.json({
-            "result": "failure", 
-            "message": "No user logged in"
-        })
-    }
+  if (!user) {
+    res.json({
+      result: "failure",
+      message: "No user logged in",
+    });
+  }
 
-    const preferences = req.body.preferences
+  const preferences = req.body.preferences;
 
-    user.set('preferences', preferences)
+  user.set("preferences", preferences);
 
-    user.save().then(()=>{
-        res.json({
-            "result": "Success"
-        })
-    }).catch((error)=>{
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
+  user
+    .save()
+    .then(() => {
+      res.json({
+        result: "Success",
+      });
     })
-})
+    .catch((error) => {
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
+});
 
 //update user trips
 
-router.put('/update/tripsCounter', (req, res) =>{
-    const user = Parse.User.current()
+router.put("/update/tripsCounter", (req, res) => {
+  const user = Parse.User.current();
 
-    user.set('tripsCounter', req.body.tripsCounter)
+  user.set("tripsCounter", req.body.tripsCounter);
 
-    user.save().then(()=>{
-        res.json({
-            "result": "Success"
-        })
-    }).catch((error)=>{
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
+  user
+    .save()
+    .then(() => {
+      res.json({
+        result: "Success",
+      });
     })
-})
+    .catch((error) => {
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
+});
 
 //update user places visited counter
-router.put('/update/placesVisited', (req, res) =>{
-    const user = Parse.User.current()
+router.put("/update/placesVisited", (req, res) => {
+  const user = Parse.User.current();
 
-    user.set('placesVisited', req.body.placesVisited)
+  user.set("placesVisited", req.body.placesVisited);
 
-    user.save().then(()=>{
-        res.json({
-            "result": "Success"
-        })
-    }).catch((error)=>{
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
+  user
+    .save()
+    .then(() => {
+      res.json({
+        result: "Success",
+      });
     })
-})
+    .catch((error) => {
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
+});
 
 //update user favorites/saved
-router.put('/update/favorites', (req, res) =>{
-    const user = Parse.User.current()
+router.put("/update/favorites", (req, res) => {
+  const user = Parse.User.current();
 
-    user.set('favorites', req.body.favorites)
+  user.set("favorites", req.body.favorites);
 
-    user.save().then(()=>{
-        res.json({
-            "result": "Success"
-        })
-    }).catch((error)=>{
-        res.json({
-            "result": "failure", 
-            "message": error
-        })
+  user
+    .save()
+    .then(() => {
+      res.json({
+        result: "Success",
+      });
     })
-})
+    .catch((error) => {
+      res.json({
+        result: "failure",
+        message: error,
+      });
+    });
+});
 
 module.exports = router;
