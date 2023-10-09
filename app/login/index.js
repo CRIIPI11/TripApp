6cd91337c819e0d256b8ed697e48f8cbedc5b92a
Sergio Montes <smcorrea19@gmail.com>
2023-10-08 20:18:39 -0400

    Main login functionality coded

--- app/login/index.js
+++ app/login/index.js
@@ -1,32 +1,85 @@
-import { StyleSheet, Image, TouchableOpacity, Text, TextInput, View } from "react-native";
+import { StyleSheet, Image, TouchableOpacity, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'; // Expo's SDK
-import { COLORS } from "../../constants";
-import { useRouter } from "expo-router";
+import { COLORS } from '../../constants';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
 
 const Login = () => {
-  const router = useRouter();
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-  return (
-    <SafeAreaView style={ styles.container }>
-        <View style={ styles.content }>
-            <Image style={styles.logo} source={require('../../assets/images/navinomad_logo.png')} />
+    const handleLogin = async () => {
+        const LOGIN_ENDPOINT = 'http://localhost:1337/Users/login'; // For local debugging
 
-            <TextInput style={ styles.input } placeholder='Email' />
-            <TextInput style={ styles.input } placeholder='Password' secureTextEntry={true} />
-            
-            <TouchableOpacity style={ styles.primaryButton }>
-                <Text style={ styles.buttonText }>Sign in</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={ styles.secondaryButton } onPress={() => {router.push('register')}}>
-                <Text style={ styles.buttonText }>Register</Text>
-            </TouchableOpacity>
-        </View>
-    </SafeAreaView>
-  );
+        const lowerEmail = email.toLowerCase();
+        console.log(`Lower email: ${lowerEmail}`);
+        try {
+            const response = await fetch(LOGIN_ENDPOINT, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: `{
+                    "email": "${lowerEmail}",
+                    "password": "${password}"
+                }`
+            });
+
+            if (!response.ok) {
+                console.log("Error during login process");
+                throw new Error('Error during login process');
+            }
+
+            const resData = await response.json();
+            console.log(resData);
+            if (resData.result === 'success') {
+                console.log("Success");
+                setErrorMessage('');
+                router.replace('discover');
+            } else {
+                console.log(`Server: ${resData.message.message}`);
+                if (resData.message.code == 101) {
+                    setErrorMessage("Email or password is incorrect, please try again");
+                } else {
+                    setErrorMessage(resData.message.message);
+                }
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Image style={styles.logo} source={require('../../assets/images/navinomad_logo.png')} />
+
+                <TextInput style={styles.input} placeholder="Email" onChangeText={setEmail} value={email} />
+                <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={setPassword} value={password} />
+
+                {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+
+                <TouchableOpacity style={styles.primaryButton}>
+                    <Text style={styles.buttonText} onPress={handleLogin}>Sign in</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.secondaryButton}
+                    onPress={() => {
+                        router.push('register');
+                    }}
+                >
+                    <Text style={styles.buttonText}>Register</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-    logo : {
+    logo: {
         alignSelf: 'center',
         width: 300,
         height: 300
@@ -59,12 +112,17 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     container: {
-        flex: 1,
+        flex: 1
     },
     content: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'center'
+    },
+    errorText: {
+        color: 'red',
+        marginBottom: 10,
+        textAlign: 'center',
     }
-})
+});
 
 export default Login;
