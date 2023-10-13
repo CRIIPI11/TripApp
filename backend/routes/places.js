b1211416fec129cfc952aa117bc4c52538ddbd05
Cristhian Molina <cdml2001@hotmail.com>
2023-10-13 11:01:06 -0400

    feat: Created text search end-point

--- backend/routes/places.js
+++ backend/routes/places.js
@@ -51,11 +51,13 @@ router.get("/location", locationLogger, (req, respos) => {
               vicinity: res.data.result?.vicinity,
               types: res.data.result?.types,
               location: res.data.result?.geometry?.location,
-              img: {
-                width: res.data.result?.photos[0].width,
-                height: res.data.result?.photos[0].height,
-                url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${res.data.result?.photos[0].photo_reference}&key=${process.env.GLE_API_KEY}`,
-              },
+              img: res.data.result?.photos
+                ? {
+                    width: res.data.result?.photos[0].width,
+                    height: res.data.result?.photos[0].height,
+                    url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${res.data.result?.photos[0].photo_reference}&key=${process.env.GLE_API_KEY}`,
+                  }
+                : null,
             });
 
             if (place.length == arr.length) {
@@ -131,11 +133,13 @@ router.get("/popular", (req, respos) => {
                 vicinity: res.data.result?.vicinity || "USA",
                 types: res.data.result?.types,
                 location: res.data.result?.geometry?.location,
-                img: {
-                  width: res.data.result?.photos[0].width,
-                  height: res.data.result?.photos[0].height,
-                  url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${res.data.result?.photos[1].photo_reference}&key=${process.env.GLE_API_KEY}`,
-                },
+                img: res.data.result?.photos
+                  ? {
+                      width: res.data.result?.photos[0].width,
+                      height: res.data.result?.photos[0].height,
+                      url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${res.data.result?.photos[1].photo_reference}&key=${process.env.GLE_API_KEY}`,
+                    }
+                  : null,
               });
               if (placesResponse.length == arr.length) {
                 console.log(placesResponse);
@@ -174,6 +178,90 @@ router.get("/popular", (req, respos) => {
   });
 });
 
+router.get("/search", searchLogger, (req, respos) => {
+  placesResponse = [];
+
+  client
+    .textSearch({
+      params: {
+        query: req.query.text,
+        // type: req.query.local === "city" ? "locality" : "tourist_attraction",
+        radius: 50000,
+        key: process.env.GLE_API_KEY,
+      },
+    })
+    .then((res) => {
+      res.data.results.map((item, i, arr) => {
+        placesResponse.push({
+          place: item.name,
+          rating: item?.rating || 4.5,
+          desc: item.editorial_summary?.overview || "no description available",
+          vicinity: item?.vicinity || "USA",
+          types: item?.types,
+          location: item?.geometry?.location,
+          img: item.photos
+            ? {
+                width: item?.photos[0]?.width,
+                height: item?.photos[0]?.height,
+                url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${item?.photos[0]?.photo_reference}&key=${process.env.GLE_API_KEY}`,
+              }
+            : null,
+          place_id: item.place_id,
+        });
+
+        if (placesResponse.length == arr.length) {
+          console.log(placesResponse);
+          respos.json({ status: "success", data: placesResponse });
+        }
+      });
+    })
+    .catch((err) => {
+      placesResponse.push({ error: "Location Cannot be Display" });
+      console.log("second call");
+      console.log(err);
+    });
+});
+
+router.get("/details", detailsLogger, (req, respos) => {
+  client
+    .placeDetails({
+      params: {
+        place_id: req.query.place_id,
+        fields: [
+          "name",
+          "editorial_summary",
+          "rating",
+          "vicinity",
+          "photo",
+          "type",
+          "geometry",
+        ],
+        key: process.env.GLE_API_KEY,
+      },
+    })
+    .then((res) => {
+      respos.json({
+        place: res.data.result?.name,
+        rating: res.data.result?.rating || 4.5,
+        desc: res.data.result.editorial_summary?.overview,
+        vicinity: res.data.result?.vicinity || "USA",
+        types: res.data.result?.types,
+        location: res.data.result?.geometry?.location,
+        img: res.data.result?.photos
+          ? {
+              width: res.data.result?.photos[0].width,
+              height: res.data.result?.photos[0].height,
+              url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${res.data.result?.photos[1].photo_reference}&key=${process.env.GLE_API_KEY}`,
+            }
+          : null,
+      });
+    })
+    .catch((err) => {
+      console.log("second call");
+      console.log(err);
+    });
+});
+
 function locationLogger(req, res, next) {
   if (req.query.latitude && req.query.longitude) {
     console.log("Logging location: ", req.query.latitude, req.query.longitude);
@@ -183,6 +271,35 @@ function locationLogger(req, res, next) {
     res.json({
       status: "failed",
       message: "No location provided.",
+    });
+  }
+}
+
+function searchLogger(req, res, next) {
+  if (
+    req.query.text
+    // && (req.query.local === "city" || req.query.local === "place")
+  ) {
+    console.log("Logging search: ", req.query.text);
+    next();
+  } else {
+    console.log("No text provided.");
+    res.json({
+      status: "failed",
+      message: "No text provided.",
+    });
+  }
+}
+
+function detailsLogger(req, res, next) {
+  if (req.query.place_id) {
+    console.log("Logging search: ", req.query.place_id);
+    next();
+  } else {
+    console.log("No place id provided.");
+    res.json({
+      status: "failed",
+      message: "No place id provided.",
     });
   }
 }
